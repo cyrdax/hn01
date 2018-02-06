6ca18d7a05906c82ab7de1e1088f35cbf72c7c2d
Reid Moran <cyrdax@gmail.com>
2018-02-06 00:53:44 -0800

    list removal

--- src/App.js
+++ src/App.js
@@ -33,6 +33,12 @@ class App extends Component {
     this.onDismiss = this.onDismiss.bind(this);
   }
 
+  onDismiss(id) {
+    const isNotId = item => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({ list: updatedList });
+  }
+
   render() {
     return (
       <div className="App">
