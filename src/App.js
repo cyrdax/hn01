90d891da30a8dea5bcd016e1401cabc28d1d1525
Reid Moran <cyrdax@gmail.com>
2018-02-07 00:24:50 -0800

    seperating components

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ class App extends Component {
 
     this.state = {
       list,
-      header: 'Welcome to React',
+      header: 'CyrdaxNotes',
       searchTerm: '',
     };
 
@@ -57,36 +57,82 @@ class App extends Component {
   }
 
   render() {
+    const { searchTerm, list } = this.state;
     return (
       <div className="App">
         <h2>{this.state.header}</h2>
-        <form>
-          <input
-            type="text"
-            onChange={this.onSearchChnage}
-          />
-        </form>
-        {this.state.list.filter(isSearched(this.state.searchTerm)).map(item =>
+        <Search
+          value={searchTerm}
+          onChange={this.onSearchChange}
+        >
+          Search
+        </Search>
+        <Table
+          list={list}
+          pattern={searchTerm}
+          onDismiss={this.onDismiss}
+        />
+      </div>
+    );
+  }
+}
+
+class Search extends Component {
+  render() {
+    const { value, onChange, children } = this.props;
+    return (
+      <form>
+        {children} <input
+          type="text"
+          value={value}
+          onChange={onChange}
+        />
+      </form>
+    )
+  }
+}
+
+class Table extends Component {
+  render() {
+    const {list, pattern, onDismiss } = this.props;
+    return (
+      <div>
+        {list.filter(isSearched(pattern)).map(item =>
           <div key={item.objectID}>
-          <span>
-            <a href={item.url}>{item.title}</a>
-          </span>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
             <span>{item.author}</span>
             <span>{item.num_comments}</span>
             <span>{item.points}</span>
             <span>
-            <button
-              onClick={() => {
-                this.onDismiss(item.objectID);
-                console.log(item.objectID);
-              }}
-              type="button"
-            >Dismiss</button>
-          </span>
+              <Button onClick={() => onDismiss(item.objectID)}>
+                Dismiss
+              </Button>
+            </span>
           </div>
-          )
-        }
+        )}
       </div>
+    )
+  }
+}
+
+class Button extends Component {
+  render() {
+    const {
+      onClick,
+      className = '',
+      children,
+    } = this.props;
+
+    return (
+      <button
+        onClick={onClick}
+        className={className}
+        type="button"
+      >
+        {children}
+      </button>
     );
   }
 }
